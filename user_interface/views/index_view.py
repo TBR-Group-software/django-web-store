303a40b4d94eb31195b1596db3f66600cdda4b97
Illia Khorunzhyi <bkmzilia@gmail.com>
2023-07-05 12:38:47 +0300

    main page and tiny fixs

--- user_interface/views/index_view.py
+++ user_interface/views/index_view.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpRequest, HttpResponse
+from django.db.models import QuerySet
+
+from product.models import ProductImage
 
 
 class IndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "base.html")
+        products = self.__get_products()
+        context = {
+            "page_title": "Shoes store",
+            "main_products": self.get_main_products(products),
+            "popular_products": self.get_post_popular_products(products),
+        }
+        return render(request, "index.html", context=context)
+
+    def get_main_products(self, products: QuerySet[ProductImage]):
+        # TODO make relevance logic
+
+        return products[:3]
+
+    def get_post_popular_products(self, products: QuerySet[ProductImage]):
+        # TODO make popular logic
+
+        return products[3:13]
+
+    def __get_products(self):
+        return ProductImage.objects.filter(
+            content_type__model="product", image_type="MAIN"
+        )
