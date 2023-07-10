a82ca3a899f2e5fd2e1181ef4c44c38a4345913f
Illia Khorunzhyi <bkmzilia@gmail.com>
2023-07-10 11:00:04 +0300

    add reviews page and revies to index page

--- user_interface/views/index_view.py
+++ user_interface/views/index_view.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest, HttpResponse
 from django.db.models import QuerySet
 
 from product.models import ProductImage
+from review.models import Review
 
 
 class IndexView(View):
@@ -13,20 +14,32 @@ class IndexView(View):
             "page_title": "Shoes store",
             "main_products": self.get_main_products(products),
             "popular_products": self.get_post_popular_products(products),
+            "reviews": self.get_post_popular_reviews(),
         }
         return render(request, "index.html", context=context)
 
-    def get_main_products(self, products: QuerySet[ProductImage]):
+    def get_main_products(
+        self, products: QuerySet[ProductImage]
+    ) -> QuerySet[ProductImage]:
         # TODO make relevance logic
 
         return products[:3]
 
-    def get_post_popular_products(self, products: QuerySet[ProductImage]):
+    def get_post_popular_products(
+        self, products: QuerySet[ProductImage]
+    ) -> QuerySet[ProductImage]:
         # TODO make popular logic
 
         return products[3:13]
 
-    def __get_products(self):
+    def get_post_popular_reviews(self) -> QuerySet[Review]:
+        # TODO make popular logic
+
+        reviews = Review.objects.all()[:3]
+
+        return reviews
+
+    def __get_products(self) -> QuerySet[ProductImage]:
         return ProductImage.objects.filter(
             content_type__model="product", image_type="MAIN"
         )
