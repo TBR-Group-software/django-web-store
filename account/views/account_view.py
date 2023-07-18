c21ab592eafcc2cf91f3107ab8f86fe5071ea724
Illia Khorunzhyi <bkmzilia@gmail.com>
2023-07-18 14:55:37 +0300

    add account info page

--- account/views/account_view.py
+++ account/views/account_view.py
@@ -1,14 +1,12 @@
 from django.views import View
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class AccountView(View):
+class AccountView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(
             request,
             "account.html",
         )
-
-    def post(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "account:account")
